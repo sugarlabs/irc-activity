sugarlabs/irc-activity

commit 99df7a3210195f86c4e611fd8a5cce4649bf5ef5

Port Gtk.TextView.get_iter_at_location() to Gtk 3.19.9
Return type was changed in the newer version 3.19.9+ to a tuple

purk/widgets.py
@@ -401,10 +401,16 @@ def word_from_pos(text, pos):
 
 
 def get_iter_at_coords(view, x, y):
-    return view.get_iter_at_location(
-        *view.window_to_buffer_coords(Gtk.TextWindowType.TEXT, int(x), int(y))
-    )
 
+    mouseClickPos = view.get_iter_at_location(
+                        *view.window_to_buffer_coords(
+                            Gtk.TextWindowType.TEXT, int(x), int(y)))
+    if Gtk.check_version(3, 19, 8) is None:
+        if not mouseClickPos[0]:
+            return False
+        mouseClickPos = mouseClickPos[1]
+
+    return mouseClickPos
 
 def get_event_at_iter(view, iter, core):
     buffer = view.get_buffer()
@@ -466,6 +472,13 @@ class TextOutput(Gtk.TextView):
 
     def set_y(self, y):
         iter = self.get_iter_at_location(0, y)
+
+        if Gtk.check_version(3, 19, 8) is None:
+            if not iter[0]:
+                return
+
+            iter = iter[1]
+
         if self.get_iter_location(iter).y < y:
             self.forward_display_line(iter)
         yalign = float(self.get_iter_location(iter).y - y) / self.height

commit 94e1ac0bfc632655ef4b3d03e2a6da9343d2b20a

Fix regression introduced by 99df7a3
There wasn't a need for the check added

Traceback shown in logs;

Traceback (most recent call last):
  File "/home/ibiam/Activities/irc-activity/purk/widgets.py", line 601, in hover
    if not hover_iter.ends_line():
AttributeError: 'bool' object has no attribute 'ends_line'

Signed-off-by: Ibiam Chihurumnaya <ibiamchihurumnaya@gmail.com>

purk/widgets.py
@@ -407,8 +407,6 @@ def get_iter_at_coords(view, x, y):
                         *view.window_to_buffer_coords(
                             Gtk.TextWindowType.TEXT, int(x), int(y)))
     if Gtk.check_version(3, 19, 8) is None:
-        if not mouseClickPos[0]:
-            return False
         mouseClickPos = mouseClickPos[1]
 
     return mouseClickPos
@@ -475,9 +473,6 @@ class TextOutput(Gtk.TextView):
         iter = self.get_iter_at_location(0, y)
 
         if Gtk.check_version(3, 19, 8) is None:
-            if not iter[0]:
-                return
-
             iter = iter[1]
 
         if self.get_iter_location(iter).y < y: